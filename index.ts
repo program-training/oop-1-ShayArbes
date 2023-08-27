f69feb84ebcbf9ac5a70709e5517809de282376d
Shay Arbes <shayarbes1@gmail.com>
2023-08-27 10:49:46 +0300

    1-3

--- index.ts
+++ index.ts
@@ -1,10 +1,50 @@
 // את הקוד שלכם תכתבו כאן
 
 //  1. Rectangle - מלבן
+class Rectangle {
+  height: number;
+  width: number;
+
+  constructor(height: number, width: number) {
+    this.height = height;
+    this.width = width;
+  }
+
+  info(): string {
+    return "This is a Recktangle";
+  }
+  getArea(): number {
+    return this.height * this.width;
+  }
+}
 
 //  2. Square - ריבוע
+class Square extends Rectangle {
+  constructor(rib: number) {
+    super(rib, rib);
+  }
+}
 
 // 3. Shape - צורה
+class Shape extends Rectangle {
+  info(): string {
+    return "This is a Shape";
+  }
+}
+
+class ColoredRectangle extends Rectangle {
+
+    color:string;
+
+    constructor(height: number, width: number , color:string) {
+        super(height, width);
+        this.color = color;
+      }
+
+    info(): string {
+      return`This is a ${this.color} Recktangle`;
+    }
+  }
 
 // 4. Method Chaining
 
